276d06110a9c734745d9f711bc88af7c7d4e5a6e
SaraToth <saratoth101@gmail.com>
2025-05-13 14:38:48 +0900

    Feat: clean up unused constants

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const indexRouter = Router();
-const messages = require("../data/messages");
 const indexController = require("../controllers/indexController");
 
 indexRouter.get("/", indexController.get);
